becd9f999a58aa4f3de63fc2e475d8363fc898fb
AmanLM <mullaaman2001@gmail.com>
2022-08-02 18:48:17 +0530

    Directory Set-up

---
+++ controllers/user_controller.js
@@ -0,0 +1,5 @@
+module.exports.Profile = function(req,res){
+    return res.render('profile',{
+        title : "Profile"
+    })
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+//Ejs-setup
+app.set('view engine', 'ejs');
+
+// For Layouts
+app.use(expressLayouts);
+
+//for styling static files
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+//static files
+app.use(express.static('./assets'));
+
+//Redirected towards routes 
+app.use('/',require('./routes'));
+
+app.listen(port,function(err){
+    if(err){
+        console.log("Error while running on PORT "+err);
+        return;
+    }
+    console.log("Running on Port : "+port);
+})
